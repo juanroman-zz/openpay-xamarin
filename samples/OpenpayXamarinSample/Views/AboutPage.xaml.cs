5cafa8ccac5979f4770dfc7b0c76d54e33cdb983
juanroman <juanroman@hotmail.com>
2018-01-04 23:02:23 -0600

    Adding Sample Projects

---
+++ samples/OpenpayXamarinSample/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace OpenpayXamarinSample
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
