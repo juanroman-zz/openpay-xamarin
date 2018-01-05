7cbb318aac61ea5933434e2fcff817b990d6eeb9
Juan Roman Escamilla <jr@jp-iusa.com>
2018-01-04 21:38:40 -0600

    Including Openpay Bindings for Android.

---
+++ src/Openpay.Xamarin.Android/OpenpayAndroidImpl.cs
@@ -0,0 +1,13 @@
+﻿using Android.App;
+using System;
+
+namespace Openpay.Xamarin.Android
+{
+    public class OpenpayAndroidImpl
+    {
+        public static void Init(Activity activity)
+        {
+            OpenpayImplementation.Activity = activity ?? throw new ArgumentNullException(nameof(activity));
+        }
+    }
+}
--- src/Openpay.Xamarin.Android/OpenpayImplementation.cs
+++ src/Openpay.Xamarin.Android/OpenpayImplementation.cs
@@ -1,21 +1,41 @@
-﻿using Openpay.Xamarin.Abstractions;
+﻿using Android.App;
+using Openpay.Xamarin.Abstractions;
 using System;
 using System.Threading.Tasks;
+using Native = MX.Openpay.Android;
 
 namespace Openpay.Xamarin
 {
     public class OpenpayImplementation : IOpenpay
     {
+        private static Native.Openpay _nativeOpenpay;
+
         public bool IsIntialized { get; private set; }
 
         public void Initialize(string merchantId, string apiKey, bool productionMode)
         {
             IsIntialized = true;
+            _nativeOpenpay = new Native.Openpay(merchantId, apiKey, new Java.Lang.Boolean(productionMode));
         }
 
         public Task<string> CreateDeviceSessionId(int timeout)
         {
-            return Task.FromResult(Guid.NewGuid().ToString());
+            // Assertions
+            CheckIfInitialized();
+            var deviceCollectorDefaultImpl = _nativeOpenpay.DeviceCollectorDefaultImpl ?? throw new InvalidOperationException("DeviceCollectorDefaultImpl is not initialized in Openpay Native API.");
+            var activity = Activity ?? throw new InvalidOperationException("Activity not defined. You need to call OpenpayAndroidImpl.Init at the beginning of your project.");
+
+            return Task.FromResult(deviceCollectorDefaultImpl.Setup(activity));
+        }
+
+        internal static Activity Activity { get; set; }
+
+        private void CheckIfInitialized()
+        {
+            if (!IsIntialized || null == _nativeOpenpay)
+            {
+                throw new InvalidOperationException("Openpay instance has not been initialized.");
+            }
         }
     }
 }
