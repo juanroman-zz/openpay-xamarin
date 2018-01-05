ed61cfdc6c55e4171744ade68ffe4a1e3a118d0f
Juan Roman Escamilla <jr@jp-iusa.com>
2018-01-05 13:29:10 -0600

    Refactoring base implementation.

--- src/Openpay.Xamarin.Android/OpenpayImplementation.cs
+++ src/Openpay.Xamarin.Android/OpenpayImplementation.cs
@@ -1,4 +1,7 @@
 ﻿using Android.App;
+using Android.Provider;
+using Android.Webkit;
+using Java.Util;
 using Openpay.Xamarin.Abstractions;
 using System;
 using System.Threading.Tasks;
@@ -7,6 +10,32 @@ namespace Openpay.Xamarin
 {
     public class OpenpayImplementation : OpenpayBaseImplementation
     {
-        
+        protected override Task<string> CreateDeviceSessionIdInternal(string merchantId, string apiKey, string baseUrl)
+        {
+            if (null == Activity)
+            {
+                throw new InvalidOperationException("Activity has not been initialized.");
+            }
+
+            var sessionId = UUID.RandomUUID().ToString();
+            sessionId = sessionId.Replace("-", string.Empty);
+
+            var identifierForVendor = Settings.Secure.GetString(Activity.ContentResolver, Settings.Secure.AndroidId);
+            var identifierForVendorScript = $"var identifierForVendor = '{identifierForVendor}';";
+
+            using (var webView = new WebView(Activity))
+            {
+                webView.SetWebViewClient(new WebViewClient());
+                webView.Settings.JavaScriptEnabled = true;
+                webView.EvaluateJavascript(identifierForVendorScript, null);
+
+                var url = $"{baseUrl}/oa/logo.htm?m={merchantId}&s={sessionId}";
+                webView.LoadUrl(url);
+
+                return Task.FromResult(url);
+            }
+        }
+
+        internal static Activity Activity { get; set; }
     }
 }
--- src/Openpay.Xamarin.iOS/OpenpayImplementation.cs
+++ src/Openpay.Xamarin.iOS/OpenpayImplementation.cs
@@ -1,21 +1,32 @@
-﻿using Openpay.Xamarin.Abstractions;
+﻿using CoreGraphics;
+using Foundation;
+using Openpay.Xamarin.Abstractions;
 using System;
 using System.Threading.Tasks;
+using UIKit;
+using WebKit;
 
 namespace Openpay.Xamarin
 {
-    public class OpenpayImplementation : IOpenpay
+    public class OpenpayImplementation : OpenpayBaseImplementation
     {
-        public bool IsIntialized { get; private set; }
-
-        public void Initialize(string merchantId, string apiKey, bool productionMode)
+        protected override async Task<string> CreateDeviceSessionIdInternal(string merchantId, string apiKey, string baseUrl)
         {
-            IsIntialized = true;
-        }
+            var sessionId = Guid.NewGuid().ToString().Replace("-", string.Empty);
 
-        public Task<string> CreateDeviceSessionId(int timeout)
-        {
-            return Task.FromResult(Guid.NewGuid().ToString());
+            var identifierForVendor = UIDevice.CurrentDevice.IdentifierForVendor.AsString().Replace("-", string.Empty);
+            var identifierForVendorScript = $"var identifierForVendor = '{identifierForVendor}';";
+
+
+            using (WKWebView webView = new WKWebView(CGRect.Empty, new WKWebViewConfiguration()))
+            {
+                await webView.EvaluateJavaScriptAsync(identifierForVendor);
+
+                var url = $"{baseUrl}/oa/logo.htm?m={merchantId}&s={sessionId}";
+                webView.LoadRequest(new NSUrlRequest(new NSUrl(url)));
+            }
+
+            return sessionId;
         }
     }
 }
