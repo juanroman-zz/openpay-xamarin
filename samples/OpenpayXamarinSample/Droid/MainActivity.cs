5e4ca6d5e67e4efd6ceaa826939a232e94b38a89
Juan Roman Escamilla <juanroman@hotmail.com>
2018-01-08 18:06:14 -0600

    Updating samples as described in README

---
+++ samples/OpenpayXamarinSample/Droid/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+
+namespace OpenpayXamarinSample.Droid
+{
+    [Activity(Label = "OpenpayXamarinSample", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            global::Acr.UserDialogs.UserDialogs.Init(this);
+            global::Openpay.Xamarin.OpenpayAndroidImpl.Init(this);
+
+            LoadApplication(new App());
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
---
+++ samples/OpenpayXamarinSample/OpenpayXamarinSample/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Openpay.Xamarin;
+using Xamarin.Forms;
+
+namespace OpenpayXamarinSample
+{
+    public partial class App : Application
+    {
+        private const string MerchantId = "mi93pk0cjumoraf08tqt";
+        private const string ApiKey = "pk_92e31f7c77424179b7cd451d21fbb771";
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Pages.MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            // Initialize Openpay
+            if (CrossOpenpay.IsSupported)
+            {
+                CrossOpenpay.Current.Initialize(MerchantId, ApiKey, false);
+            }
+        }
+    }
+}
