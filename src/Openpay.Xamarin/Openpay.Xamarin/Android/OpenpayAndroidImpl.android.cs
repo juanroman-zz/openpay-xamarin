d8b5fbe85b2a586637320079dc9f6b0abc22e475
Juan Roman Escamilla <jr@jp-iusa.com>
2018-12-19 16:00:16 -0600

    Switching to multi-library project.

---
+++ src/Openpay.Xamarin/Openpay.Xamarin/Android/OpenpayAndroidImpl.android.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using System;
+
+namespace Openpay.Xamarin
+{
+    /// <summary>
+    /// Helper class for Android initialization.
+    /// </summary>
+    public static class OpenpayAndroidImpl
+    {
+        /// <summary>
+        /// Android specific initializer used to keep a reference of the Main Activity.
+        /// </summary>
+        /// <param name="activity">The main application's <see cref="Activity"/> instance.</param>
+        public static void Init(Activity activity) => OpenpayImplementation.Activity = activity ?? throw new ArgumentNullException(nameof(activity));
+    }
+}
--- src/Openpay.Xamarin/Openpay.Xamarin/Apple/OpenpayImplementation.apple.cs
+++ src/Openpay.Xamarin/Openpay.Xamarin/Apple/OpenpayImplementation.apple.cs
@@ -8,8 +8,17 @@ using WebKit;
 
 namespace Openpay.Xamarin
 {
+    /// <summary>
+    /// <see cref="OpenpayBaseImplementation"/> implentation for iOS.
+    /// </summary>
     public class OpenpayImplementation : OpenpayBaseImplementation
     {
+        /// <summary>
+        /// Implementación por/plataforma de la solicitud del identificador de la sesión.
+        /// </summary>
+        /// <param name="merchantId">El identificador del cliente</param>
+        /// <param name="apiKey">La llave pública del API del cliente</param>
+        /// <param name="baseUrl">El URL al que se debe conectar la plataforma.</param>
         protected override Task<string> CreateDeviceSessionIdInternal(string merchantId, string apiKey, string baseUrl)
         {
             var sessionId = Guid.NewGuid().ToString().Replace("-", string.Empty);
